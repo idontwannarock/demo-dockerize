d5fa748489eb94f63bf35a712fef01486b379876
Howard Wang <idontwannarock@gmail.com>
2019-07-19 09:58:31 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Intro
+
+Base project of a dockerized Spring Boot application with MySQL database.
+
+# Quick Start
+
---
+++ src/test/java/com/example/demodockerize/DemoDockerizeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demodockerize;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoDockerizeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
